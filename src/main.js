f0f641174a6bd8a80a538bd12e5529aca9cf0f3a
Dylan Hobbs <dylan@dylanhobbs.ie>
2021-08-30 22:59:57 +0300

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './assets/scss/app.scss';
+
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
